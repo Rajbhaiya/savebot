de8934438db615ca04fa90eb0bcab6f41349af33
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-12-09 18:19:25 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = "13675555"
+API_HASH = "c0da9c346d2c45dbc7ec49a05da9b2b6"
+BOT_TOKEN = "5555986769:AAG-nNw82PHwBPlPZ5h55d3hnfHxzqc5JeI"
+SESSION = "AQBHfvhz_GMIwsKIfOn_KYlptj5dx1Wtj4b8vBjM4lNkVsFQRiEVCMeADnURo2Rha7x9gutooWTeUwgmdjx0_IlxgBwrmvVoFH6dO_CVtRTESnkWBPt3LfWhdOZSu-338WQ5OCbm_wP3Sx8xfmpgkqzHVUScmTvCmIp5TH0DA766QDunwoKrUgYIS-GicjgHJWiemUZJVb96D0Ygo0HzAUGTspLQSUtY4_fAVXvwNY_R8MhfhdR7DXEfuAWinVmJ5P9MO9bqohsTl01y1x60_iZb6RW8LPBMSInH8w7Lr2jAVMEAd-xqpQh9yih1lKC5NSF4WbRhfYMJG5TTjvWkUJj9AAAAATkBGUIA"
+FORCESUB = "Ecchi_world_K"
+AUTH = "5591954930"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
