dade426d26d74a91c2426905ec60306a0145adea
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-12-09 20:26:23 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,7 @@
-echo "starting Bot ~@DroneBots";
+echo "[INFO]: BOT STARTED";
+cd /app
+rm -rf savebot1
+git clone https://github.com/Rajbhaiya/savebot1
+cd savebot1
+pip install --quiet -r requirements.txt
 python3 -m main
